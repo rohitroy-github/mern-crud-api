9ba06ae2eff70868fcc573bf95c81d430d583a5a
rohit <rhtry.tech@gmail.com>
2023-02-08 02:16:07 +0530

    adding files !

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log("Hi, there !");
+// initializingExpress
+const express = require("express");
+
+const dotenv = require("dotenv").config();
+
+// importingErrorHandler
+const {errorHandler} = require("./middleware/errorMiddleware");
+
+// declarePort
+const port = process.env.PORT || 5000;
+
+// declaringExpressApp
+const app = express();
+
+// expressMiddlewaresForDataReadability
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+// overwritingDefaultExpressErrorHandler
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server up & running at http://localhost:${port}/`);
+});
