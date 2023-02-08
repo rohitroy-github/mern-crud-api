ebfd2222eaf7ef3dde7926995bb7bb466f754a00
rohit <rhtry.tech@gmail.com>
2023-02-09 00:11:48 +0530

    adding files !

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,15 @@
 // asyncHandlerForMongooseHandling
 const asyncHandler = require("express-async-handler");
 
+// importingModels
+const Goal = require("../models/goalModels");
+
 // functionToGetGoals
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({message: "Get goals !"});
+  // findingAllAvailableGoals
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
 });
 
 // functionToSetGoals
@@ -13,18 +19,49 @@ const setGoals = asyncHandler(async (req, res) => {
     // expressErrorHandler
     throw new Error("Please add a text field data ! ");
   }
+
+  // createANewGoal
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
   //   console.log(req.body);
-  res.status(200).json({message: "Set goal !"});
+  res.status(200).json(goal);
 });
 
 // functionToUpdateGoals
 const updateGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Update goal ${req.params.id}`});
+  // fetchingGoalWithIDForCheckingAvailibility
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found !");
+  }
+
+  // fetchingGoalAndUpdating
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
 });
 
 // functionToDeleteGoals
 const deleteGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({message: `Delete goal ${req.params.id}`});
+  // fetchingGoalWithIDForCheckingAvailibility
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found !");
+  }
+
+  // deletingGoal
+  goal.remove();
+
+  // displayingDeletedGoalId
+  res.status(200).json({id: req.params.id});
 });
 
 module.exports = {
