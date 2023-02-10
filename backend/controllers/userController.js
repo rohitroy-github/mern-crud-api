1d01856941a4c7f606e9cfac905f8e80009a43d2
rohit <rhtry.tech@gmail.com>
2023-02-10 12:25:28 +0530

    updates !

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,14 +7,69 @@ const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 
 const User = require("../models/userModel");
+const {user} = require("../routes/userRoutes");
 
 // functionForRegisteringNewUser
 const registerUser = asyncHandler(async (req, res) => {
-  res.json({message: "Register user !"});
+  // destructuringData
+  const {name, email, password} = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please fill up all the fields !");
+  }
+
+  // checkIfUserAlreadyExistsUsingEmailProperty?
+  const userExists = await User.findOne({email});
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists !");
+  }
+
+  // hashPasswordUsingbcrypt
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // createUser
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  // ifUserIsCreated?
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data !");
+  }
+  // res.json({message: "Register user !"});
 });
 
 // functionForLoginUser
 const loginUser = asyncHandler(async (req, res) => {
+  const {email, password} = req.body;
+
+  const user = await User.findOne({email});
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials !");
+  }
   res.json({message: "Login user !"});
 });
 
@@ -22,6 +77,14 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({message: "User !"});
 });
+
+// functionToGenerateToken
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    // tokenExpiresIn10Days
+    expiresIn: "10d",
+  });
+};
 
 module.exports = {
   registerUser,
