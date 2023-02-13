c099924567beb8ccd05c4472de827a135de65c3f
rohit <rhtry.tech@gmail.com>
2023-02-13 19:32:10 +0530

    adding file !

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,6 +3,15 @@
 import React from "react";
 import {useState, useEffect} from "react";
 import {FaSignInAlt} from "react-icons/fa";
+// forUsingReduxFunctions
+import {useSelector, useDispatch} from "react-redux";
+import {login, reset} from "../features/auth/authSlice";
+// forRedirecting
+import {useAsyncError, useNavigate} from "react-router-dom";
+// forMakingToasts
+import {toast} from "react-toastify";
+
+import Spinner from "../components/Spinner";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -11,6 +20,25 @@ const Login = () => {
   });
 
   const {email, password} = formData;
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -21,7 +49,18 @@ const Login = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+
+    dispatch(login(userData));
   };
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   return (
     <>
